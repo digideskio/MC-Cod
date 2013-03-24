7155178b8511060dfde1e5d6b6da267629fbea64
Alex Godwin <agodwin987@gmail.com>
2013-03-24 13:45:51 -0400

    Added utilities

---
+++ src/Util/ChatHelpers.java
@@ -0,0 +1,8 @@
+package Util;
+
+import org.bukkit.ChatColor;
+
+public class ChatHelpers {
+	public static String codInfo = ChatColor.GOLD + "[MC-COD Info]" + ChatColor.DARK_BLUE+" ";
+	public static String codError = ChatColor.GOLD + "[MC-COD Error]" + ChatColor.DARK_RED+" ";
+}
---
+++ src/Util/Data.java
@@ -0,0 +1,12 @@
+package Util;
+
+import java.io.File;
+
+public class Data {
+	public void SaveData(File file){
+		
+	}
+	public void LoadDdata(File file){
+		
+	}
+}
