da612b650d1159cb7ca8ff0e510020bfdfbcc69b
Alex Godwin <agodwin987@gmail.com>
2013-03-23 20:37:12 -0400

    Fixed the location for entity collision

--- src/JBCod/CarePackageHandler.java
+++ src/JBCod/CarePackageHandler.java
@@ -4,7 +4,6 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Chest;
 import org.bukkit.entity.Egg;
-import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.ProjectileHitEvent;
